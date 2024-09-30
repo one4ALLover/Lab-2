74784de1ff812884d69f159e41df2f97edb381fc
one4ALLover <egorrvanijked@gmail.com>
2024-09-30 10:01:48 +0300

    task_2_new_t_done

--- main.cpp
+++ main.cpp
@@ -5,20 +5,10 @@
 #include <time.h>
 #include <Windows.h>
 
-const int size = 100000;
-int randarr1[size], randarr2[size], randarr3[size];
-int vozrast1[size], vozrast2[size], vozrast3[size];
-int ubiv1[size], ubiv2[size], ubiv3[size];
-int vozubiv1[size], vozubiv2[size], vozubiv3[size];
-
+const int size[] = { 1000, 100000 };
 int k = 0;
-double vremya[12];
+double vremya[24];
 
-//—Å—Ç–∞—Ç–∏—á –º–∞—Å—Å –ø–µ—Ä–µ–¥–µ–ª –≤ –¥–∏–Ω–∞–º–∏—á–µ—Å–∫ + –ø–æ—Å–º—Ç–æ—Ä–µ—Ç—å —Ä–∞–∑–Ω–∏—Ü—É –≤–æ –≤—Ä–µ–º–µ–Ω–∏ –º–µ–∂–¥—É 100000 –∏ 1000
-int* randarr1= (int*)malloc(size * sizeof(int)), *randarr2= (int*)malloc(size * sizeof(int)), *randarr3= (int*)malloc(size * sizeof(int));
-int* vozrast1= (int*)malloc(size * sizeof(int)), *vozrast2= (int*)malloc(size * sizeof(int)), *vozrast3= (int*)malloc(size * sizeof(int));
-int* ubiv1= (int*)malloc(size * sizeof(int)), *ubiv2= (int*)malloc(size * sizeof(int)), *ubiv3= (int*)malloc(size * sizeof(int));
-int* vozubiv1= (int*)malloc(size * sizeof(int)), *vozubiv2= (int*)malloc(size * sizeof(int)), *vozubiv3= (int*)malloc(size * sizeof(int));
 void zaparr(int* a, int* b, int* c, int* d, int size)
 {
     int i;
@@ -27,7 +17,7 @@ void zaparr(int* a, int* b, int* c, int* d, int size)
         b[i] = i;
         c[i] = size - i;
         if (i < (size / 2)) { d[i] = i; }
-        else { d[i] = size/2 - i - 1; }
+        else { d[i] = size - i - 1; }
     }
 
 }
@@ -58,13 +48,13 @@ void shell(int* items, int count)
     }
 }
 
-void qs(int* items, int left, int right) //–≤—ã–∑–æ–≤ —Ñ—É–Ω–∫—Ü–∏–∏: qs(items, 0, count-1);
+void qs(int* items, int left, int right) //‚˚ÁÓ‚ ÙÛÌÍˆËË: qs(items, 0, count-1);
 {
     int i, j;
     int x, y;
     i = left; j = right;
 
-    /* –≤—ã–±–æ—Ä –∫–æ–º–ø–∞—Ä–∞–Ω–¥–∞ */
+    /* ‚˚·Ó ÍÓÏÔ‡‡Ì‰‡ */
     x = items[(left + right) / 2];
 
     do {
@@ -79,15 +69,21 @@ void qs(int* items, int left, int right) //–≤—ã–∑–æ–≤ —Ñ—É–Ω–∫—Ü–∏–∏: qs(items, 
         }
     } while (i <= j);
 
-    if (left < j) qs(items, left, j);
-    if (i < right) qs(items, i, right);
+    if (j - left < right - i) {
+        if (left < j) qs(items, left, j);
+        left = i;
+    }
+    else {
+        if (i < right) qs(items, i, right);
+        right = j;
+    }
 }
 
 int compare(const void* a, const void* b) {
     return (*(int*)a - *(int*)b);
 }
 
-void schetShell(int* a, int* k)
+void schetShell(int* a, int* k, int size)
 {
     clock_t start = clock();
     shell(a, size);
@@ -96,7 +92,7 @@ void schetShell(int* a, int* k)
     (*k)++;
 }
 
-void schetQs(int* a, int* k)
+void schetQs(int* a, int* k, int size)
 {
     clock_t start = clock();
     qs(a, 0, size - 1);
@@ -105,7 +101,7 @@ void schetQs(int* a, int* k)
     (*k)++;
 }
 
-void schetQsort(int* a, int* k)
+void schetQsort(int* a, int* k, int size)
 {
     clock_t start = clock();
     qsort(a, size, sizeof(int), compare);
@@ -114,40 +110,66 @@ void schetQsort(int* a, int* k)
     (*k)++;
 }
 
-
 int main() {
 
     SetConsoleCP(1251);
     SetConsoleOutputCP(1251);
 
     srand(time(NULL));
+    for (int d = 0; d < 2; d++)
+    {
+        int* randarr1 = (int*)malloc(size[d] * sizeof(int)), * randarr2 = (int*)malloc(size[d] * sizeof(int)), * randarr3 = (int*)malloc(size[d] * sizeof(int));
+        int* vozrast1 = (int*)malloc(size[d] * sizeof(int)), * vozrast2 = (int*)malloc(size[d] * sizeof(int)), * vozrast3 = (int*)malloc(size[d] * sizeof(int));
+        int* ubiv1 = (int*)malloc(size[d] * sizeof(int)), * ubiv2 = (int*)malloc(size[d] * sizeof(int)), * ubiv3 = (int*)malloc(size[d] * sizeof(int));
+        int* vozubiv1 = (int*)malloc(size[d] * sizeof(int)), * vozubiv2 = (int*)malloc(size[d] * sizeof(int)), * vozubiv3 = (int*)malloc(size[d] * sizeof(int));
 
-    zaparr(randarr1, vozrast1, ubiv1, vozubiv1, size);
+        zaparr(randarr1, vozrast1, ubiv1, vozubiv1, size[d]);
 
-    kopirka(&randarr1[size], &randarr2[size], &randarr3[size], size);
-    kopirka(&vozrast1[size], &vozrast2[size], &vozrast3[size], size);
-    kopirka(&ubiv1[size], &ubiv2[size], &ubiv3[size], size);
-    kopirka(&vozubiv1[size], &vozubiv2[size], &vozubiv3[size], size);
+        kopirka(randarr1, randarr2, randarr3, size[d]);
+        kopirka(vozrast1, vozrast2, vozrast3, size[d]);
+        kopirka(ubiv1, ubiv2, ubiv3, size[d]);
+        kopirka(vozubiv1, vozubiv2, vozubiv3, size[d]);
 
-    schetShell(randarr1, &k);
-    schetShell(vozrast1, &k);
-    schetShell(ubiv1, &k);
-    schetShell(vozubiv1, &k);
+        schetShell(randarr1, &k, size[d]);
+        schetShell(vozrast1, &k, size[d]);
+        schetShell(ubiv1, &k, size[d]);
+        schetShell(vozubiv1, &k, size[d]);
 
-    schetQs(randarr2, &k);
-    schetQs(vozrast2, &k);
-    schetQs(ubiv2, &k);
-    schetQs(vozubiv2, &k);
-    
-    schetQsort(randarr3, &k);
-    schetQsort(vozrast3, &k);
-    schetQsort(ubiv3, &k);
-    schetQsort(vozubiv3, &k);
+        schetQs(randarr2, &k, size[d]);
+        schetQs(vozrast2, &k, size[d]);
+        schetQs(ubiv2, &k, size[d]);
+        schetQs(vozubiv2, &k, size[d]);
 
+        schetQsort(randarr3, &k, size[d]);
+        schetQsort(vozrast3, &k, size[d]);
+        schetQsort(ubiv3, &k, size[d]);
+        schetQsort(vozubiv3, &k, size[d]);
+
+        printf("–‡ÁÏÂ Ï‡ÒÒË‚Ó‚ %d:\n", size[d]);
+        printf("       |  rand |  voz  |  ub   |  vu   |\n-----------------------------------------\n");
+        printf(" shell | %3.3lf | %3.3lf | %3.3lf | %3.3lf |\n-----------------------------------------\n", vremya[0 + 12 * d], vremya[1 + 12 * d], vremya[2 + 12 * d], vremya[3 + 12 * d]);
+        printf("   qs  | %3.3lf | %3.3lf | %3.3lf | %3.3lf |\n-----------------------------------------\n", vremya[4 + 12 * d], vremya[5 + 12 * d], vremya[6 + 12 * d], vremya[7 + 12 * d]);
+        printf(" qsort | %3.3lf | %3.3lf | %3.3lf | %3.3lf |\n-----------------------------------------\n\n", vremya[8 + 12 * d], vremya[9 + 12 * d], vremya[10 + 12 * d], vremya[11 + 12 * d]);
+
+        free(randarr1);
+        free(randarr2);
+        free(randarr3);
+        free(vozrast1);
+        free(vozrast2);
+        free(vozrast3);
+        free(ubiv1);
+        free(ubiv2);
+        free(ubiv3);
+        free(vozubiv1);
+        free(vozubiv2);
+        free(vozubiv3);
+    }
+
+    printf("–‡ÁÌÓÒÚ¸ 100000 Ë 1000:\n");
     printf("       |  rand |  voz  |  ub   |  vu   |\n-----------------------------------------\n");
-    printf(" shell | %3.3lf | %3.3lf | %3.3lf | %3.3lf |\n-----------------------------------------\n", vremya[0], vremya[1], vremya[2], vremya[3]);
-    printf("   qs  | %3.3lf | %3.3lf | %3.3lf | %3.3lf |\n-----------------------------------------\n", vremya[4], vremya[5], vremya[6], vremya[7]);
-    printf(" qsort | %3.3lf | %3.3lf | %3.3lf | %3.3lf |\n-----------------------------------------\n", vremya[8], vremya[9], vremya[10], vremya[11]);
+    printf(" shell | %3.3lf | %3.3lf | %3.3lf | %3.3lf |\n-----------------------------------------\n", vremya[12] - vremya[0], vremya[13] - vremya[1], vremya[14] - vremya[2], vremya[15] - vremya[3]);
+    printf("   qs  | %3.3lf | %3.3lf | %3.3lf | %3.3lf |\n-----------------------------------------\n", vremya[16] - vremya[4], vremya[17] - vremya[5], vremya[18] - vremya[6], vremya[19] - vremya[7]);
+    printf(" qsort | %3.3lf | %3.3lf | %3.3lf | %3.3lf |\n-----------------------------------------\n\n", vremya[20] - vremya[8], vremya[21] - vremya[9], vremya[22] - vremya[10], vremya[23] - vremya[11]);
 
     return 0;
 }
